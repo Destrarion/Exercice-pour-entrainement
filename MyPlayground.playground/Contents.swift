2a49bb49eeb113639925fb3f1c8b3c409c292460
Fabien Dietrich <destrarion@gmail.com>
2018-02-15 17:25:57 +0100

    Je viens de commit le dossier avec le playground, il y contient le hello world et le commentaire du compteur que j'appellerais Ouinon

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+/*
+ Ici je vais commencer donc a créer un petit programme qui va compter les oui et les non en fonction de la commande executer dans le terminal. Il y aura 4 bouton :
+ 1 - Le bouton qui va ajouter +1 a la variable oui
+ 2 - Le bouton qui va ajouter +1 a la variable non
+ 3 - Le bouton qui va afficher le nombre de oui ou de non avec les statistiques
+ 4 - le bouton qui va permettre de remettre tout à 0
+ */
